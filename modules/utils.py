28edc5cfa90fd6cdd46a0c1fd216a4a4e9125212
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-02-01 23:45:08 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -86,13 +86,19 @@ async def progress_bar(current, total, reply, start):
             sp = str(hrb(speed)) + "/s"
             tot = hrb(total)
             cur = hrb(current)
+            
+            #don't even change anything till here
+            # Calculate progress bar dots
+            #ab mila dil ko sukun #by rajput
+            #change from here if you want 
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "▓" * completed_length + "▒" * remaining_length
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ ๛𝗠𝗥๛𝗥͜͡𝗔𝗝𝗣𝗨𝗧๛ ⌋────╯`\n') 
-         #       await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈  𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ๛𝗠𝗥๛𝗥͜͡𝗔𝗝𝗣𝗨𝗧๛ ⌋─╯`\n') 
+                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ 𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ๛𝗠𝗥๛𝗥͜͡𝗔𝗝𝗣𝗨𝗧๛ ⌋────╯`\n') 
+         #       await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈ 𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ๛𝗠𝗥๛𝗥͜͡𝗔𝗝𝗣𝗨𝗧๛ ⌋─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
+    
