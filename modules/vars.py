a0dfc7e186bedfbdd65aa2dfaa9a0496dac82845
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-02-04 07:30:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24490468"
 api_hash = "0e72faecb7ab5fcce79603941e1a8889"
-bot_token = "6865551919:AAFni4H0Zn7N_ZtJGC2gHkhrDitNlYkenkM"
+bot_token = "6865551919:AAEKYQ_eWD0GVklgN5bU0Wlaf3MmDexDfzU"
