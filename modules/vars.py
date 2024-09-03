46493c2815d1a61f9776533c5c7c5b29b53d7a3d
PRADEEP999901 <149851567+PRADEEP999901@users.noreply.github.com>
2024-09-03 16:09:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "21103068"
 api_hash = "633ab5bfab52d8763ffc6da7a9b2294a"
-bot_token = "7151352201:AAGowxDTAV-irri_zuOUu8P7fVN4rnqj9EM"
+bot_token = "7151352201:AAG-irri_zuOUu8P7fVN4rnqj9EM"
